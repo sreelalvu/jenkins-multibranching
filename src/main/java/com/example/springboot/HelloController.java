d98e30b6cd88e4e73fcea30eb0582d1720279fe3
satyakumr <71006158+satyakumr@users.noreply.github.com>
2021-07-13 12:00:22 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot jhgujggj !!";
+		return "Greetings from Spring Boot asdasdasdsd !!";
 	}
 
 }
